acfb3f1079a669855045f158880733025e1c9ebf
wxjqgt <wwwm1905com>
2017-08-29 09:34:52 +0800

    配置数据库

---
+++ app/src/main/java/com/weibo/coolweather/model/db/City.java
@@ -0,0 +1,49 @@
+package com.weibo.coolweather.model.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by weixj on 2017/8/29.
+ */
+
+public class City extends DataSupport {
+
+    private int id;
+    private String cityName;
+    private int cityCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+
+    private int provinceId;
+
+}
